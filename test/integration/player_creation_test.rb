bc47337e40404ad29016b7d8d66366110c488f70
William Bland <williamlbland@yahoo.co.uk>
2019-04-25 14:33:04 +0100

    tests

---
+++ test/integration/player_creation_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PlayerCreationTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "can see the homepage" do
+    get "/"
+    assert_select "h1", "Welcome#index"
+  end
+
+  test "add a player" do
+    get "/player/new"
+    assert_response :success
+
+    post "/articles",
+      params: { article: { title: "can create", body: "article successfully." } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "p", "Title:\n  can create"
+  end
+
+end
